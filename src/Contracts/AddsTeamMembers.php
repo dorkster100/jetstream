0536f2ae6e613bc42f3b3bfb89a65d8d839c102c
dorkster100 <dorkster100@gmail.com>
2024-08-30 00:33:34 +0300

    Fix phpstan error

--- src/Contracts/AddsTeamMembers.php
+++ src/Contracts/AddsTeamMembers.php
@@ -3,7 +3,7 @@
 namespace Laravel\Jetstream\Contracts;
 
 /**
- * @method void add(\Illuminate\Foundation\Auth\User $user, \Illuminate\Database\Eloquent\Model $team, string $email, string $role = null)
+ * @method void add(\Illuminate\Foundation\Auth\User|null $user, \Illuminate\Database\Eloquent\Model $team, string $email, string $role = null)
  */
 interface AddsTeamMembers
 {
